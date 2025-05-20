f5ee506384d434b5ca434272ba9d245fca6830ab
shmuelgross <shmuelgross3@gmail.com>
2025-05-20 16:15:05 +0300

    methods result static

---
+++ ConsoleApp3/UppDateStudent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    static class UppDateStudent
+    {
+        public static void UppDateStudentResult(Student student)
+        {
+            if (student.Grade >= 90)
+            {
+                student.result = "Excellent";
+            }
+            else if (student.Grade >= 75)
+            {
+                student.result = "Good";
+            }
+            else if (student.Grade >= 60)
+            {
+                student.result = "Pass";
+            }
+            else
+            {
+                student.result = "Fail";
+            }
+        }
+    }
+}
