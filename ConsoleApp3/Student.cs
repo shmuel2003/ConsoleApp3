c4564c94af875b4fbac559a8523c9d7af2dca4cc
shmuelgross <shmuelgross3@gmail.com>
2025-05-20 15:57:47 +0300

    methods static

---
+++ ConsoleApp3/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Student
+    {
+        // internal
+        public string Name;
+        public int Grade;
+        public string result;
+    }
+    static class UpdateStudent
+    {
+        public static void UpdateStudentName(Student student)
+        {
+            Console.WriteLine("Enter student's name");
+            student.Name = Console.ReadLine();
+        }
+        public static void UpdateStudentGrade(Student student)
+        {
+            Console.WriteLine("Enter student's grade");
+            student.Grade = int.Parse(Console.ReadLine());
+        }
+    }
+}
